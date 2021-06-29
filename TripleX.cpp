ea5b2291a37130440b89aa62f02f21298c962581
DanielYankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-06-29 13:00:44 -0400

    const and assigning values in C++ fundamentals

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,13 +5,13 @@ int main()
     std::cout << "You are a secret agent breaking into a server room...";
     std::cout << std::endl;
     std::cout << "You need to enter the correct codes to continue...";
+    
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
 
-    int a = 4;
-    int b = 3;
-    int c = 2;
-
-    int sum = a + b + c; 
-    int product = a * b * c; 
+    const int sum = a + b + c; 
+    const int product = a * b * c; 
 
     std::cout << std::endl;
     std::cout << sum << std::endl;
