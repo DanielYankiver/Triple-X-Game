d42b7d5e668960785ab42c7b79dfda9cce96faba
DanielYankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-06-29 13:37:18 -0400

    created a new variable called PlayerGuess

--- TripleX.cpp
+++ TripleX.cpp
@@ -7,7 +7,7 @@ int main()
     std::cout << std::endl;
     std::cout << "You need to enter the correct codes to continue..." << std::endl;
     
-    // Declare 3 number code 
+    // Declare a 3 number code 
     const int CodeA = 4;
     const int CodeB = 3;
     const int CodeC = 2;
@@ -15,11 +15,13 @@ int main()
     const int CodeSum = CodeA + CodeB + CodeC; 
     const int CodeProduct = CodeA * CodeB * CodeC; 
  
-    // Print sum and product to the terminal 
+    // Print CodeSum and CodeProduct to the terminal 
     std::cout << std::endl;
     std::cout << "+ There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add up to: " << CodeSum << std::endl;
     std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl; 
+
+    int PlayerGuess;
 
     return 0; 
 } 
